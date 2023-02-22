cece82ac1a94afb740199c72a1e74ac48deb5b2b
AlmiraEfremenko <almira.efremenko@yandex.ru>
2023-02-22 10:56:20 +0500

    Add greetingViewController.

--- MorgeUIkit/Source/GreetingModule/Controller/GreetingViewController.swift
+++ MorgeUIkit/Source/GreetingModule/Controller/GreetingViewController.swift
@@ -5,4 +5,31 @@
 //  Created by MAC on 17.02.2023.
 //
 
-import Foundation
+import UIKit
+
+class GreetingViewController: UIViewController {
+
+    private let singInView = SingInViewController()
+
+    private var greetingView: GreetingView? {
+        guard isViewLoaded else { return nil }
+        return view as? GreetingView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view = GreetingView()
+        greetingsUser()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            let quizWelcomeViewController = QuizWelcomeViewController()
+            quizWelcomeViewController.modalPresentationStyle = .fullScreen
+            self.present(quizWelcomeViewController, animated: false)
+        }
+    }
+
+    private func greetingsUser() {
+        guard let name = UserSettings.userModel?.name else { return }
+        greetingView?.greetingsLabel.text = "Привет, \(name)!"
+    }
+}
