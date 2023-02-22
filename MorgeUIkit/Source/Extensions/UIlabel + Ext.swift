e1223ec0d4fb5e3376ff74e9fd5aa3b07a938ca1
AlmiraEfremenko <almira.efremenko@yandex.ru>
2023-02-22 10:56:35 +0500

    Add UILabel + Ext

--- MorgeUIkit/Source/Extensions/UIlabel + Ext.swift
+++ MorgeUIkit/Source/Extensions/UIlabel + Ext.swift
@@ -5,4 +5,17 @@
 //  Created by MAC on 21.02.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    convenience init(text: String = "", font: UIFont?) {
+        self.init()
+        self.text = text
+        self.font = font
+        self.textColor = .black
+        self.adjustsFontSizeToFitWidth = true
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.adjustsFontSizeToFitWidth = true
+        self.numberOfLines = 3
+    }
+}
