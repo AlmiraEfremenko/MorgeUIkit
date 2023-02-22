916a64000e5ab4311db701933292399c32681656
AlmiraEfremenko <almira.efremenko@yandex.ru>
2023-02-22 10:55:30 +0500

    Add singInModule.

--- MorgeUIkit/Source/SingInModule/Controller/SingInViewController.swift
+++ MorgeUIkit/Source/SingInModule/Controller/SingInViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class SingInViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    private var singInView: SingInView? {
+        guard isViewLoaded else { return nil }
+        return view as? SingInView
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view = SingInView()
 
+        singInView?.singIn = { [weak self] in
+            guard let name = self?.singInView?.nameTextField.text else { return }
+            let userModel = UserModel(name: name)
+            UserSettings.userModel = userModel
+            self?.signInAccount()
+        }
+    }
+    
+    private func signInAccount() {
+        let greetingViewController = GreetingViewController()
+        greetingViewController.modalPresentationStyle = .fullScreen
+        self.present(greetingViewController, animated: false)
+    }
 }
-
--- MorgeUIkit/Source/SingInModule/Model/UserModel.swift
+++ MorgeUIkit/Source/SingInModule/Model/UserModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class UserModel: NSObject, NSCoding {
+
+    let name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+    }
+
+    required init?(coder: NSCoder) {
+        name = coder.decodeObject(forKey: "name") as? String ?? ""
+    }
+}
