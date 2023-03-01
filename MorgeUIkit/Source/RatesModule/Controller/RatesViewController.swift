390857f18fb81d9e0a6f2597b95d15f6059b6905
AlmiraEfremenko <almira.efremenko@yandex.ru>
2023-03-01 14:12:49 +0500

    Create RatesModule.

---
+++ MorgeUIkit/Source/RatesModule/Controller/RatesViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RatesViewController.swift
+//  MorgeUIkit
+//
+//  Created by MAC on 27.02.2023.
+//
+
+import Foundation
---
+++ MorgeUIkit/Source/RatesModule/View/RatesView.swift
@@ -0,0 +1,8 @@
+//
+//  RatesView.swift
+//  MorgeUIkit
+//
+//  Created by MAC on 27.02.2023.
+//
+
+import Foundation
